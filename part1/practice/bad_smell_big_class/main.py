ca452ee2f25177c8229684708cb4bfeaebc2d51c
kukla <kuklajonny@bk.ru>
2023-01-31 13:58:11 +0530

    Женя Рубцова

--- part1/practice/bad_smell_big_class/main.py
+++ part1/practice/bad_smell_big_class/main.py
@@ -1,49 +1,39 @@
-# в задании представлен один класс который нужно разбить на 4
-# Воин      - Warrior
-# Лекарь    - Healer
-# Дерево    - Tree
-# Ловушка   - Trap
-
-# Воин в состоянии защищаться от врагов, атаковать и передвигаться по полю
-# Лекарь может защищаться, лечить воина и панически спасаться бегством
-# Дерево может защищаться (попробуй разруби сходу) и гореть в огне
-# Ловушка не может ничего кроме как атаковать того, кто на нее наступит
-# Для решения этой задачи не используйте наследование
-
-class Obj:
-    ##
-    # тут представлено поведение четырех различных игровых объектов:
-    # - воина
-    # - лекаря
-    # - дерева
-    # - ловушки
+class Warrior:  # Воин
 
     def attack(self):
-        pass
-
-    def defense(self):
         pass
 
     def move(self):
         pass
 
-    def healer_defense(self):
+    def defense(self):  # защита
         pass
 
-    def healer_move(self):
+
+class Healer:  # Лекарь
+    def heal(self):  # лечить
         pass
 
-    def heal(self):
+    def move(self):
         pass
 
-    def tree_defense(self):
+    def defense(self):  # защита
+        pass
+
+
+class Tree:
+
+    def defense(self):  # защита
         pass
 
     def on_fire(self):
         pass
 
-    def trap_attack(self):
-        print("It's a trap!")
+
+class Trap:  # Ловушка
+    def attack(self):
+        pass
+
 
 if __name__ == '__main__':
     unit = Warrior()
